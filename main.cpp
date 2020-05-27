fd2cde57979e6088b18e51c2550dce45afafc38a
hubfra <51798356+hubfra@users.noreply.github.com>
2020-05-27 23:51:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,59 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
 
 int main()
 {
-	int left_boundary;
-	int right_boundary;
-	int primes_count = 0;
+	int input_down_boundary;
+	int input_top_boundary;
 
-	cout << "Left bounadry: ";
-	cin >> left_boundary;
-	cout << "Right boundary: ";
-	cin >> right_boundary;
+	std::cout << "Down bounadry: ";
+	std::cin >> input_down_boundary;
+	std::cout << "Top boundary: ";
+	std::cin >> input_top_boundary;
 
-	vector<int> primes(sqrt(right_boundary) + 1, 1);
+	int real_down_boundary = std::max(input_down_boundary, 0);
+	int real_top_boundary = std::max(input_top_boundary, 0);
+
+	if (real_down_boundary > real_top_boundary) {
+		std::swap(real_down_boundary, real_top_boundary);
+	}
+
+	std::vector<int> primes(sqrt(real_top_boundary) + 1, 1);
 	primes[0] = 0;
 	primes[1] = 0;
 
-	for (int a = 2; a < primes.size(); a++)
-	{
-		if (primes[a] == 1)
-		{
-			for (int b = a * 2; b < primes.size(); b += a)
-			{
+	int sq = sqrt(primes.size());
+	for (int a = 2; a < sq; ++a) {
+		if (primes[a]) {
+			for (int b = a * a; b < primes.size(); b += a) {
 				primes[b] = 0;
 			}
 		}
 	}
 
-	for (int a = left_boundary >= 2 ? left_boundary : 2; a <= right_boundary; a++)
-	{
-		bool is_prime = true;
 
-		for (int b = 2; b <= sqrt(a); b++)
-		{
-			if (primes[b] == 1)
-			{
-				if (a % b == 0)
-				{
-					is_prime = false;
-					break;
+	int primes_count = 0;
+
+	auto isPrime = [&primes](int n) {
+		int sqrt_n = sqrt(n);
+		for (int a = 2; a <= sqrt_n; ++a) {
+			if (primes[a]) {
+				if (n % a == 0) {
+					return false;
 				}
 			}
 		}
+		return true;
+	};
 
-		if (is_prime)
-		{
-			primes_count++;
-		}
+	for (int a = std::max(real_down_boundary, 2); a <= real_top_boundary; ++a) {
+		if (isPrime(a)) primes_count++;
 	}
 
-	cout << "Between " << left_boundary << " and " << right_boundary << " there are " << primes_count << " primes\n";
+	std::cout << "There are " << primes_count << " primes between " << input_down_boundary << " and " << input_top_boundary << '\n';
 
 	return 0;
 }
